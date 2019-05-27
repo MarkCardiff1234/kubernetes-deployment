9e5b57a564fcf17e7b552ff57e30257ad40c1d41
Mark Cardiff <Mark.Cardiff@gmail.com>
2019-05-27 10:24:41 +0000

    everything worls

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,5 @@
-FROM nginx:latest
+
+FROM php:7-fpm
 COPY www /usr/share/nginx/html
+RUN docker-php-ext-install mysqli
+
--- www/index.php
+++ www/index.php
@@ -32,7 +32,7 @@
 				<h1>Hello World!</h1>
 				<h4>Attempting MySQL connection from php...</h4>
 				<?php
-				$host = 'db-service';
+				$host = 'db';
 				$user = 'root';
 				$pass = 'rootpassword';
 				$conn = new mysqli($host, $user, $pass);
